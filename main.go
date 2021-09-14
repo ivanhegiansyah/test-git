a34e775ad71c2fd26859791ab99da87ce46ee746
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-09-14 23:00:03 +0800

    menambah test 1-4

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("testing git")
+func main() {
+	fmt.Println("test 1")
+	fmt.Println("test 2")
+	fmt.Println("test 3")
+	fmt.Println("test 4")
 }
